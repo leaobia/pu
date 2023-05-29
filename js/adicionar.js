18c73429453c56279a99660f9771b9b4b2721523
Bianca Leão <22282176@CFP127ALUNO.LOCAL>
2023-05-29 16:01:06 -0300

    dados já sendo diferenciados

--- js/adicionar.js
+++ js/adicionar.js
@@ -4,7 +4,7 @@ const criarDadosHorario = () => {
 
     const container_horario = document.getElementById('container-horario')
 
-    const container_dados = document.createElement('div')
+    const container_dados = document.createElement('form')
     container_dados.classList.add('dados')
     container_dados.id = 'dados'
 
@@ -17,27 +17,17 @@ const criarDadosHorario = () => {
     const date_input = document.createElement('input')
     date_input.type = 'date'
     date_input.classList.add('date_input')
-    date_input.id = 'date_input'
+    //date_input.id = 'date_input'
     date_input.classList.add('check_input')
+    date_input.required = true
 
 
     const label_calendar = document.createElement('label')
     label_calendar.classList.add('fas')
     label_calendar.classList.add('fa-calendar-alt')
     label_calendar.for = date_input
-    label_calendar.id = 'label_calendar'
+   // label_calendar.id = 'label_calendar'
 
-    label_calendar.addEventListener('click', () => {
-        date_input.showPicker();
-    })
-
-    date_input.addEventListener('change', () => {
-        const myInputCalendarValue = date_input.value;
-        const myInputCalendarValueBR = date_input.value.substring(8, 10) + '/' + date_input.value.substring(5, 7);
-        valorData.textContent = myInputCalendarValueBR
-        console.log(myInputCalendarValue);
-        console.log(myInputCalendarValueBR);
-    });
 
 
     const inicio = document.createElement('div')
@@ -48,25 +38,18 @@ const criarDadosHorario = () => {
     const time_input_inicio = document.createElement('input')
     time_input_inicio.type = 'time'
     time_input_inicio.classList.add('time_input_inicio')
-    time_input_inicio.id = 'time_input_inicio'
+   // time_input_inicio.id = 'time_input_inicio'
     time_input_inicio.classList.add('check_input')
+    time_input_inicio.required = true
 
     const label_time_inicio = document.createElement('label')
     label_time_inicio.classList.add('fas')
     label_time_inicio.classList.add('fa-clock')
     label_time_inicio.classList.add('label_time')
     label_time_inicio.for = time_input_inicio
-    label_time_inicio.id = 'label_time_inicio'
+   // label_time_inicio.id = 'label_time_inicio'
 
-    label_time_inicio.addEventListener('click', () => {
-        time_input_inicio.showPicker();
-    })
 
-    time_input_inicio.addEventListener('change', () => {
-        const myInputValue = time_input_inicio.value;
-        valorInicio.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
-        console.log(myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5));
-    })
 
     const termino = document.createElement('div')
     termino.classList.add('termino')
@@ -75,26 +58,21 @@ const criarDadosHorario = () => {
     time_input_termino.type = 'time'
     time_input_termino.classList.add('time_input_termino')
     time_input_termino.classList.add('check_input')
-    time_input_termino.id = 'time_input_termino'
+    time_input_termino.required = true
+    //time_input_termino.id = 'time_input_termino'
 
 
     const valorTermino = document.createElement('i')
 
-    time_input_termino.addEventListener('change', () => {
-        const myInputValue = time_input_termino.value;
-        valorTermino.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
-    })
+
 
     const label_time_termino = document.createElement('label')
     label_time_termino.classList.add('fas')
     label_time_termino.classList.add('fa-clock')
     label_time_termino.classList.add('label_time')
     label_time_termino.for = time_input_termino
-    label_time_termino.id = 'label_time_termino'
+    //label_time_termino.id = 'label_time_termino'
 
-    label_time_termino.addEventListener('click', () => {
-        time_input_termino.showPicker();
-    })
 
     const desconto = document.createElement('div')
     desconto.classList.add('desconto')
@@ -104,25 +82,18 @@ const criarDadosHorario = () => {
     const time_input_desconto = document.createElement('input')
     time_input_desconto.type = 'time'
     time_input_desconto.classList.add('time_input_desconto')
-    time_input_desconto.id = 'time_input_desconto'
+    //time_input_desconto.id = 'time_input_desconto'
     time_input_desconto.classList.add('check_input')
-    time_input_desconto.value = '01:14'
+    time_input_desconto.required = true
 
     const label_time_desconto = document.createElement('label')
     label_time_desconto.classList.add('fas')
     label_time_desconto.classList.add('fa-clock')
     label_time_desconto.classList.add('label_time')
     label_time_desconto.for = time_input_desconto
-    label_time_desconto.id = 'label_time_termino'
+    //label_time_desconto.id = 'label_time_termino'
 
-    time_input_desconto.addEventListener('change', () => {
-        const myInputValue = time_input_desconto.value;
-        valorDesconto.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
-    })
 
-    label_time_desconto.addEventListener('click', () => {
-        time_input_desconto.showPicker();
-    })
 
 
     const liquido = document.createElement('div')
@@ -151,6 +122,64 @@ const criarDadosHorario = () => {
     button_excluir.id = 'excluir'
 
 
+    // click e change
+
+    
+    label_calendar.addEventListener('click', () => {
+        date_input.showPicker();
+    })
+
+
+    label_time_desconto.addEventListener('click', () => {
+        time_input_desconto.showPicker();
+    })
+
+    
+    label_time_inicio.addEventListener('click', () => {
+        time_input_inicio.showPicker();
+    })
+
+    label_time_termino.addEventListener('click', () => {
+        time_input_termino.showPicker();
+    })
+
+
+
+    date_input.addEventListener('change', () => {
+        const myInputCalendarValue = date_input.value;
+        const myInputCalendarValueBR = date_input.value.substring(8, 10) + '/' + date_input.value.substring(5, 7);
+        valorData.textContent = myInputCalendarValueBR
+        //console.log(myInputCalendarValue);
+       // console.log(myInputCalendarValueBR);
+    });
+
+    time_input_termino.addEventListener('change', () => {
+        const myInputValue = time_input_termino.value;
+        valorTermino.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
+    })
+
+
+
+    time_input_inicio.addEventListener('change', () => {
+        const myInputValue = time_input_inicio.value;
+        valorInicio.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
+        //console.log(myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5));
+    })
+
+
+    time_input_desconto.addEventListener('change', () => {
+        const myInputValue = time_input_desconto.value;
+
+        const formatoBack = myInputValue + ':00'
+        
+        valorDesconto.textContent = myInputValue.substring(0, 2) + 'h' + myInputValue.substring(3, 5)
+    })
+
+
+
+
+    // colocando no pai
+
     container_horario.append(container_dados)
     container_dados.append(data, inicio, termino, desconto, liquido, total_geral, dados_botoes)
     data.append(label_calendar, date_input, valorData)
@@ -162,39 +191,38 @@ const criarDadosHorario = () => {
     dados_botoes.append(button_excluir, button_editar)
 
 
-
-    const inputs = document.querySelectorAll('.check_input');
-    let inputsAlterados = Array.from(inputs).fill(false); //
-    console.log(inputsAlterados);
-
     const btc_save = document.getElementById('salvar')
+    const inputs = document.querySelectorAll('.check_input');
+    console.log(inputs);
+    
 
-    const habilitarBotaoSalvar = () => {
-        const todosAlterados = inputsAlterados.every((alterado) => alterado);
-        if (todosAlterados) {
-            btc_save.classList.add('d-flex');
-            btc_save.classList.remove('d-none');
-        }
-    };
-
-    inputs.forEach((input, index) => {
+    inputs.forEach((input) => {
         input.addEventListener('input', () => {
-            inputsAlterados[index] = true;
-            habilitarBotaoSalvar();
+            const form = document.querySelectorAll('.dados')
+
+            form.forEach((dadoContainer) => {
+                if(dadoContainer.checkValidity()){
+                    btc_save.classList.add('d-flex');
+                    btc_save.classList.remove('d-none');
+                    console.log(dadoContainer.checkValidity());
+                }else{
+                    console.log('Não foi possível salvar os dados, por conta de não ser preenchido todos os inputs');
+                    console.log(dadoContainer.checkValidity());
+                }
+            })
+            
+
         });
     });
 
-    //   const desabilitarInputs = () => {
-    //     inputs.forEach((input) => {
-    //       input.disabled = true;
-    //     });
-    //   };
+
+
 
 
 
 
     btc_save.addEventListener('click', () => {
-        // desabilitarInputs()
+    
         if (time_input_termino.value == null, time_input_termino == "", time_input_inicio == null, time_input_inicio == " ", time_input_termino < time_input_inicio) {
             console.log('Não foi possivel calcular');
         } else {
@@ -218,14 +246,14 @@ const criarDadosHorario = () => {
                 somaMinutes -= 60;
             }
 
-            console.log(`${somaHours}h${somaMinutes}`);
+            //console.log(`${somaHours}h${somaMinutes}`);
 
             const valorTotal = `${somaHours}h${somaMinutes}`
             const valorTotalLiquid = `${somaHoursLiquid}h${somaMinutesLiquid}`
 
 
             if (valorTotal == '0hNaN' || valorTotalLiquid == '0hNaN') {
-                console.log('Ainda nnn');
+                console.log('Não foi possível processar os valores.');
 
             } else {
                 valor_total.textContent = valorTotal.replace('-', '0')
@@ -238,7 +266,7 @@ const criarDadosHorario = () => {
             btc_save.classList.remove('d-flex')
 
         }
-        //inputsAlterados[index].classList.add('alterado')
+
     })
 
     button_excluir.addEventListener('click', () => {
